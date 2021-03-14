f48efa88837a40f14262a442df0634b7844092f8
SherstnevVasiliy <root@kola-nn.com>
2021-03-15 00:43:34 +0300

    fix: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketItem = '' ;
+  let i= 0;
+  while (i < bracketsConfig.length) {
+    bracketItem = bracketsConfig[i].join('');
+
+      if (str.includes(bracketItem)) {
+          str = str.replace(bracketItem,'');
+          i = 0;
+      } else {
+          i += 1;
+      }
+  }
+
+  return str.length === 0;
 }
